d3bbf74d83574ec8bfaf895f2dcfc1ca838b0940
vlaleks <mrr999399@gmail.com>
2021-10-15 01:15:30 +1000

    Lesson 3: Add news model, migration, factory and seeder

---
+++ database/seeders/NewsSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use \App\Models\News;
+
+class NewsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        News::query()->delete();
+        News::factory(random_int(15, 25))->create();
+    }
+}
